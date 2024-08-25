4022f24593b2733096fc12621829f7eed7d4fabc
Blumenkohl6 <92886335+Blumenkohl6@users.noreply.github.com>
2024-08-25 02:35:11 +0200

    email verifiication added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const { google } = require('googleapis');
 const dotenv = require('dotenv');
 const crypto = require('crypto');
+const axios = require('axios');
 
 dotenv.config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const MAIL_API_KEY = process.env.MAIL_API_KEY;
+const MAIL_SECRET_KEY = process.env.MAIL_SECRET_KEY;
+const MAIL = process.env.MAIL;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const ROOT_FOLDER = process.env.ROOT_FOLDER;
@@ -120,6 +124,73 @@ app.get('/auth/:authToken', async(req, res) => {
 
     res.status(200).json({ userData: publicUser });
 });
+
+app.get('/user/verify/:email/:verificationToken', async(req, res) => {
+    // Entgegennehmen der Daten
+    const { email, verificationToken } = req.params;
+
+    // Überprüfen, ob alle erforderlichen Daten vorhanden sind
+    if (!email || !verificationToken) {
+        return res.status(400).json({ error: 'email & verificationToken sind erforderlich.' });
+    }
+
+    const data = await verifyUser(email);
+
+    if (!data) {
+        return res.status(400).json({ error: 'Email ist nicht gültig.' });
+    }
+
+    if (data.token !== verificationToken) {
+        return res.status(400).json({ error: 'VerificationToken ist nicht gültig.' });
+    }
+
+    if (data.timeStamp < Date.now() - 1000 * 60 * 60 * 24) {
+        return res.status(400).json({ error: 'VerificationToken ist abgelaufen.' });
+    }
+
+    createUser(email, data.username, data.password)
+
+    deleteVerificationToken(email);
+
+    res.status(200).json({ message: 'User wurde erfolgreich erstellt & verifiziert.' });
+});
+
+async function verifyUser(email) {
+    // Suche die Datei auf Google Drive
+    const response = await drive.files.list({
+        q: `name='VerificationTokens.json' and '1iBaR0z6LeNa6nV3tVy_FfZlNQkpYjWro' in parents and trashed=false`,
+        fields: 'files(id, name)',
+        pageSize: 1, // Limitierung auf nur 1 Ergebnis
+    });
+
+    const files = response.data.files;
+
+    if (files.length) {
+        const file = files[0];
+        const data = await parseFile(file);
+
+        return data[email];
+    }
+}
+
+async function deleteVerificationToken(email) {
+    // Suche die Datei auf Google Drive
+    const response = await drive.files.list({
+        q: `name='VerificationTokens.json' and '1iBaR0z6LeNa6nV3tVy_FfZlNQkpYjWro' in parents and trashed=false`,
+        fields: 'files(id, name)',
+        pageSize: 1, // Limitierung auf nur 1 Ergebnis
+    });
+
+    const files = response.data.files;
+
+    if (files.length) {
+        const file = files[0];
+        const data = await parseFile(file);
+
+        data[email] = undefined;
+        updateFile(file.id, JSON.stringify(data));
+    }
+}
 
 app.delete('/user/:uuid/:username/:password', async(req, res) => {
     // Entgegennehmen der Daten
@@ -243,6 +314,8 @@ app.post('/user/:email/:username/:password', async(req, res) => {
         return res.status(400).json({ error: 'Email wurde bereits verwendet.' });
     }
 
+    sendVerificationEmail(email, username, password);
+    /*
     const uuid = await createUser(email, username, password);
 
     const publicUser = getPublicUser(await getUserFromFile(uuid + ".json"), uuid);
@@ -252,6 +325,8 @@ app.post('/user/:email/:username/:password', async(req, res) => {
     saveAuthToken(authToken, uuid);
 
     res.status(200).json({ authToken: authToken, userData: publicUser });
+    */
+    res.status(200).json({});
 });
 
 async function createUser(email, username, password) {
@@ -420,5 +495,98 @@ async function updateFile(fileId, newContent) {
         });
     } catch (error) {
         console.error('Fehler beim Aktualisieren der Datei:', error);
+    }
+}
+
+async function sendVerificationEmail(email, name, password) {
+    const verificationToken = generateToken();
+
+    await saveVerificationToken(email, verificationToken, name, password);
+
+    const data = JSON.stringify({
+        "Messages": [{
+            "From": { "Email": MAIL, "Name": "Whacking Wizards" },
+            "To": [{ "Email": email, "Name": name }],
+            "Subject": "Verify Your Email Address",
+            "HTMLPart": `
+            <div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; margin: auto;">
+                <h2 style="color: #444;">Hello ${name},</h2>
+                <p>Welcome to the Whacking Wizards community! We're thrilled to have you on board.</p>
+                <p>Before you can start using your account, please verify your email address by clicking the button below:</p>
+                <div style="text-align: center; margin: 20px 0;">
+                    <form action="http://localhost:8080/user/verify/${email}/${verificationToken}" method="POST" style="display: inline;">
+                        <button type="submit" 
+                                style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px; border: none; cursor: pointer;">
+                            Verify Email
+                        </button>
+                    </form>
+                </div>
+            <p>If the button above doesn't work, please copy and paste the following link into your web browser:</p>
+            <p><a href="http://localhost:8080/user/verify/${email}/${verificationToken}">http://localhost:8080/user/verify/${email}/${verificationToken}</a></p>
+            <p>If you did not request this, please ignore this email. Your account will not be activated without verification.</p>
+            <p>Best regards,<br>The Whacking Wizards Team</p>
+        </div>
+        `,
+            "TextPart": `Hello ${name},
+
+Welcome to the Whacking Wizards community! We're thrilled to have you on board.
+
+Before you can start using your account, please verify your email address by clicking the link below:
+http://localhost:8080/user/verify/${email}/${verificationToken}
+
+If you did not request this, please ignore this email. Your account will not be activated without verification.
+
+Best regards,
+The Whacking Wizards Team`
+        }]
+    });
+
+    const config = {
+        method: 'post',
+        url: 'https://api.mailjet.com/v3.1/send',
+        data: data,
+        headers: { 'Content-Type': 'application/json' },
+        auth: { username: MAIL_API_KEY, password: MAIL_SECRET_KEY },
+    };
+
+    return axios(config)
+        .then(function(response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+}
+
+async function saveVerificationToken(email, token, username, password) {
+    //save new token
+    try {
+        // Suche die Datei auf Google Drive
+        const response = await drive.files.list({
+            q: `name='VerificationTokens.json' and '1iBaR0z6LeNa6nV3tVy_FfZlNQkpYjWro' in parents and trashed=false`,
+            fields: 'files(id, name)',
+            pageSize: 1, // Limitierung auf nur 1 Ergebnis
+        });
+
+        const files = response.data.files;
+
+        if (files.length) {
+            const file = files[0];
+            const data = await parseFile(file);
+
+            //delete old token
+            for (const key in data) {
+                if (data[key] === email) {
+                    delete data[key];
+                }
+            }
+
+            data[email] = { token: token, timeStamp: Date.now(), username: username, password: password };
+            updateFile(file.id, JSON.stringify(data));
+        }
+    } catch (error) {
+        console.error('Error downloading file:', error);
+        return false;
     }
 }
